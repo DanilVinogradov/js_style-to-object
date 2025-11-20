00f8cf39c40b6ed1407efa0407c0d2703ad0119b
Arramis <danilvino228@gmail.com>
2025-11-20 12:47:27 +0200

    convert

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,32 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const obj = {};
+  const lines = sourceString.split(';');
+
+  for (let line of lines) {
+    line = line.trim();
+
+    if (!line) {
+      continue;
+    }
+
+    const index = line.lastIndexOf(':');
+
+    if (index === -1) {
+      continue;
+    }
+
+    const key = line.slice(0, index).trim();
+    let value = line.slice(index + 1).trim();
+
+    if (value.endsWith(';')) {
+      value = value.slice(0, -1).trim();
+    }
+    obj[key] = value;
+  }
+
+  return obj;
 }
 
 module.exports = convertToObject;
